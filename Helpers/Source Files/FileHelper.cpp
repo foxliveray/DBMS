3b3db708fb7062a4336b383477339e3f677c579f
lixww <1374541937@qq.com>
2017-05-09 11:46:22 +0800

    Update FileHelper.cpp

--- Helpers/Source Files/FileHelper.cpp
+++ Helpers/Source Files/FileHelper.cpp
@@ -1,38 +1,65 @@
 //文件操作辅助类FileHelper实现文件
 #include "stdafx.h"
-#include <fileapi.h>
+#include "AppException.h"
 #include "FileHelper.h"
 
 //创建文件
 bool FileHelper::CreateFile(const CString fileName)
 {
-	//创建文件夹
-	for (int i = 0; i < fileName.GetLength(); i++)
+	try
 	{
-		if (fileName.GetAt(i) == _T('/') || fileName.GetAt(i) == _T('\\'))
+		//创建文件夹
+		for (int i = 0; i < fileName.GetLength(); i++)
 		{
-			CString fileDirectory;
-			fileDirectory = fileName.Left(i);
-			if (!CreateDirectory(fileDirectory, NULL))
-				return false;
+			if (fileName.GetAt(i) == _T('/') || fileName.GetAt(i) == _T('\\'))
+			{
+				CString fileDirectory;
+				fileDirectory = fileName.Left(i);
+				if (!CreateDirectory(fileDirectory, NULL))
+					return false;
+			}
 		}
+
+		//创建文件
+		CFile newFile;
+		if (!newFile.Open(fileName, CFile::modeCreate))
+			return false;
+		newFile.Close();
+
+		return true;
+	} //异常处理：创建文件失败
+	catch (CException* e)    
+	{
+		e->Delete();
+		throw new CAppException(_T("创建文件失败！"));
+	}
+	catch (...)
+	{
+		throw new CAppException(_T("创建文件失败！"));
 	}
 
-	//创建文件
-	CFile newFile;
-	if (!newFile.Open(fileName, CFile::modeCreate))
-		return false;
-	newFile.Close();
-
-	return true;
-
-	//异常处理：创建文件失败
+	return false;
 }
 
 //判断文件是否合法
 bool FileHelper::IsVaildFile(const CString fileName)
 {
-	CFileFind finder;
-	bool isFileExist = finder.FindFile(fileName);
-	return isFileExist;
+	try
+	{
+		CFileFind finder;
+		bool isFileExist = finder.FindFile(fileName);
+
+		return isFileExist;
+	} //异常处理：检查文件时出错
+	catch (CException* e)      
+	{
+		e->Delete();
+		throw new CAppException(_T("检查文件时出错！"));
+	}
+	catch (...)
+	{
+		throw new CAppException(_T("检查文件时出错！"));
+	}
+
+	return false;
 }
