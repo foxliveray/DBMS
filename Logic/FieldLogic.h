8182aabb4d0f74f2001f5195065a3d65a606feb1
zhumengting <444638290@qq.com>
2017-05-15 19:23:06 +0800

    Add files via upload

---
+++ Logic/FieldLogic.h
@@ -0,0 +1,23 @@
+#pragma once
+#include"CFieldEntity.h"
+#include "FileHelper.h"
+#include "global.h"
+#include"FieldDao.h"
+#include"FileLogic.h"
+class FieldLogic
+{
+public:
+	FieldLogic();
+	~FieldLogic();
+	bool CreateField(const CString dbName, const CString tbName, CFieldEntity & fd);
+	//创建数据库
+	//查询数据库
+	bool getField(const CString dbName,const CString tbName,CFieldEntity &fd);
+	bool getField(const CString dbName, const CString tbName,CString oldName,CString newName);
+	bool getField(const CString dbName, const CString tbName, CString oldName,int newtype);
+	bool getField(const CString dbName, const CString tbName, CString oldName);
+	vector<CFieldEntity> GetFieldList(const CString dbName, const CString tbName);
+private:
+	FieldDao fieldDao;
+	FileLogic filelogic;	//数据库文件的逻辑对象
+};
